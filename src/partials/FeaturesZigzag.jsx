3e7721acdce6a1fccb6376d2cb59cd6b280c2e23
Nihal Londhe <nihallondhe20@gmail.com>
2023-03-22 10:49:39 +0530

    changes in im

--- src/partials/FeaturesZigzag.jsx
+++ src/partials/FeaturesZigzag.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import FeatImage01 from '../images/features-03-image-01.png';
-import FeatImage02 from '../images/features-03-image-02.png';
+import FeatImage02 from '../images/features-03-image-01.png';
 import FeatImage03 from '../images/features-03-image-03.png';
 
 function FeaturesZigzag() {
